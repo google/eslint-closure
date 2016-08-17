e397b76efa57a23d03f9694958f30788df29f58c
Joe Schafer <jschaf@google.com>
2016-08-16 23:42:10 -0700

    feat: add test for camelcase-optionals

---
+++ tests/lib/rules/camelcase-optionals.js
@@ -0,0 +1,133 @@
+/**
+ * @fileoverview Tests for camelcase-optionals rule.
+ * @author Gregg Tavares (but it's just a modified version of camelcase by Nicholas C. Zakas)
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/camelcase-optionals');
+const RuleTester = require('eslint').RuleTester;
+const ruleTester = new RuleTester();
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+ruleTester.run('camelcase-optionals', rule, {
+  valid: [
+    'firstName = "Nicholas"',
+    'FIRST_NAME = "Nicholas"',
+    '__myPrivateVariable = "Patrick"',
+    'myPrivateVariable_ = "Patrick"',
+    'function doSomething(){}',
+    'do_something()',
+    'foo.do_something()',
+    'var foo = bar.baz_boom;',
+    'var foo = bar.baz_boom.something;',
+    'foo.boom_pow.qux = bar.baz_boom.something;',
+    'if (bar.baz_boom) {}',
+    'var obj = { key: foo.bar_baz };',
+    'var arr = [foo.bar_baz];',
+    '[foo.bar_baz]',
+    'var arr = [foo.bar_baz.qux];',
+    '[foo.bar_baz.nesting]',
+    'if (foo.bar_baz === boom.bam_pow) { [foo.baz_boom] }',
+    'var a = opt_test;',
+    'var args = var_args;',
+  ],
+  invalid: [
+    {
+      code: 'first_name = "Nicholas"',
+      errors: [
+        {
+          message: "Identifier 'first_name' is not in camel case.",
+          type: 'Identifier',
+        },
+      ],
+    },
+    {
+      code: '__private_first_name = "Patrick"',
+      errors: [
+        {
+          message: "Identifier '__private_first_name' is not in camel case.",
+          type: 'Identifier',
+        },
+      ],
+    },
+    {
+      code: 'function foo_bar(){}',
+      errors: [
+        {
+          message: "Identifier 'foo_bar' is not in camel case.",
+          type: 'Identifier',
+        },
+      ],
+    },
+    {
+      code: 'obj.foo_bar = function(){};',
+      errors: [
+        {
+          message: "Identifier 'foo_bar' is not in camel case.",
+          type: 'Identifier',
+        },
+      ],
+    },
+    {
+      code: 'bar_baz.foo = function(){};',
+      errors: [
+        {
+          message: "Identifier 'bar_baz' is not in camel case.",
+          type: 'Identifier',
+        },
+      ],
+    },
+    {
+      code: '[foo_bar.baz]',
+      errors: [
+        {
+          message: "Identifier 'foo_bar' is not in camel case.",
+          type: 'Identifier',
+        },
+      ],
+    },
+    {
+      code: 'if (foo.bar_baz === boom.bam_pow) { [foo_bar.baz] }',
+      errors: [
+        {
+          message: "Identifier 'foo_bar' is not in camel case.",
+          type: 'Identifier',
+        },
+      ],
+    },
+    {
+      code: 'foo.bar_baz = boom.bam_pow',
+      errors: [
+        {
+          message: "Identifier 'bar_baz' is not in camel case.",
+          type: 'Identifier',
+        },
+      ],
+    },
+    {
+      code: 'var foo = { bar_baz: boom.bam_pow }',
+      errors: [
+        {
+          message: "Identifier 'bar_baz' is not in camel case.",
+          type: 'Identifier',
+        },
+      ],
+    },
+    {
+      code: 'foo.qux.boom_pow = { bar: boom.bam_pow }',
+      errors: [
+        {
+          message: "Identifier 'boom_pow' is not in camel case.",
+          type: 'Identifier',
+        },
+      ],
+    },
+
+  ],
+});
