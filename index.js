d996beb82b370504bfe19d92e9072140d442562a
Joe Schafer <jschaf@google.com>
2016-08-17 23:34:19 -0700

    feat: register rules

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    'line-end-spaced-comment': require('./lib/rules/line-end-spaced-comment.js'),
+    'camelcase-optionals': require('./lib/rules/camelcase-optionals.js'),
+  },
+};
