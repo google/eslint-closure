5b2c3fdb7bd3d6d248b80817697e74d2f9e6a782
Joe Schafer <jschaf@google.com>
2016-09-03 23:35:18 -0700

    feat(plugin): add commonjs extern

---
+++ packages/eslint-plugin-googlejs/externs/externs-commonjs.js
@@ -0,0 +1,20 @@
+
+/** @fileoverview An extern file for commonjs modules.  We take this approach
+ * because we don't want to typecheck all node_modules.  I only care about the
+ * provided externs file.  This works because we're not outputting or minifying
+ * anything.  We only want typechecking.
+ * @externs
+ */
+
+/** @type {!Object} */
+var module;
+
+/** @type {*} */
+module.exports;
+
+
+/**
+ * @param {string} src
+ * @return {!Object}
+ */
+function require(src) {}
