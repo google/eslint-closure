5f81726e65c9d89917f1325bf09802997660383b
Joe Schafer <jschaf@google.com>
2016-09-05 03:03:49 -0700

    fix(plugin): get mocha and chai working

--- packages/eslint-plugin-googlejs/tests/lib/utils.js
+++ packages/eslint-plugin-googlejs/tests/lib/utils.js
@@ -2,4 +2,25 @@
  * @fileoverview Tests for utils.
  */
 
-const utils = require('../lib/utils');
+/* global describe it expect */
+const utils = require('../../lib/utils');
+const expect = require('chai').expect
+describe('Utils', () => {
+
+  it('should correctly determine underscored strings', () => {
+    expect(utils.isUnderscored('')).to.be.false;
+    expect(utils.isUnderscored('a')).to.be.false;
+    expect(utils.isUnderscored('ab')).to.be.false;
+    expect(utils.isUnderscored('ab c')).to.be.false;
+
+
+    expect(utils.isUnderscored('_')).to.be.true;
+    expect(utils.isUnderscored('a_')).to.be.true;
+    expect(utils.isUnderscored('__')).to.be.true;
+    expect(utils.isUnderscored('_a')).to.be.true;
+    expect(utils.isUnderscored(' _')).to.be.true;
+    expect(utils.isUnderscored('abc _')).to.be.true;
+    expect(utils.isUnderscored('_abc')).to.be.true;
+  })
+
+});
