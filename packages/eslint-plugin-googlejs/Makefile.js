4eada785d4d419b19160a30c3c94f3291f3bf78d
Joe Schafer <jschaf@google.com>
2016-09-18 12:33:09 -0700

    refactor(plugin): plead with closure to work

--- packages/eslint-plugin-googlejs/Makefile.js
+++ packages/eslint-plugin-googlejs/Makefile.js
@@ -6,130 +6,165 @@
 /* eslint no-use-before-define: "off", no-console: "off" */
 'use strict';
 
-require("shelljs/make");
+require('shelljs/make');
 
 const closurePackage = require('google-closure-compiler');
 const ClosureCompiler = closurePackage.compiler;
-const nodeCLI = require("shelljs-nodecli");
+const nodeCLI = require('shelljs-nodecli');
 
 /* eslint-disable googlejs/camelcase */
 
 
-const NODE_MODULES = "./node_modules/",
+const NODE_MODULES = './node_modules/',
 
     // Utilities - intentional extra space at the end of each string.
-    MOCHA = NODE_MODULES + "mocha/bin/_mocha ",
-    ESLINT = "eslint ",
+  MOCHA = NODE_MODULES + 'mocha/bin/_mocha ',
+  ESLINT = 'eslint ',
 
     // Files
-    MAKEFILE = "./Makefile.js",
-    JS_FILES = "lib/**/*.js",
-    TEST_FILES = "tests/lib/rules/**/*.js tests/lib/*.js",
+  MAKEFILE = './Makefile.js',
+  JS_FILES = 'lib/**/*.js',
+  TEST_FILES = 'tests/lib/rules/**/*.js tests/lib/*.js',
 
     // Settings
-    MOCHA_TIMEOUT = 10000;
+  MOCHA_TIMEOUT = 10000;
 
-const closureCompiler = new ClosureCompiler(
-  {
-    js: [
-      'index.js',
-      "'./lib/**.js'",
-      // "'./tests/**.js'",
-    ],
-    externs: [
-      './externs/externs-eslint.js',
-      './externs/externs-espree.js',
-      // './externs/externs-commonjs.js',
-    ],
-    language_in: 'ECMASCRIPT6_STRICT',
-    warning_level: 'VERBOSE',
-    jscomp_error: "'*'",
-    // We use null for options that don't have a value.  Otherwise, it errors
-    // out.  The existence of 'checks-only' is enough for it to be included as
-    // an option.
-    checks_only: null,
-    // process_common_js_modules: null,
-    new_type_inf: null,
-    assume_function_wrapper: null,
-    // We need LOOSE because we might as well type check all files.  If we used
-    // STRICT, closure would ignore all files it couldn't reach from the
-    // entry_point.
-    dependency_mode: 'STRICT',
-    entry_point: 'googlejs.config',
-    // output_manifest: 'manifest.MF',
-    // debug: null,
-  },
-  // Java options.
-  [
-  ]
-);
+const commonClosureCompilerSettings = {
+  js: [
+    'index.js',
+    "'./lib/**.js'",
+  ],
+  externs: [
+    './externs/externs-commonjs.js',
+    './externs/externs-eslint.js',
+    './externs/externs-espree.js',
+  ],
+  language_in: 'ECMASCRIPT6_STRICT',
+  language_out: 'ECMASCRIPT5_STRICT',
+  warning_level: 'VERBOSE',
+  jscomp_error: "'*'",
+  // We use null for options that don't have a value.  Otherwise, it errors
+  // out.  The existence of 'checks-only' is enough for it to be included as
+  // an option.
+  new_type_inf: null,
+  // assume_function_wrapper: null,
+  dependency_mode: 'STRICT',
+  entry_point: 'googlejs.config',
+};
 
-
+const closureJavaOptions = [];
 
 target.all = function() {
   target.test();
 };
 
 target.checkTypes = function() {
-  closureCompiler.run(function(exitCode, stdout, stderr) {
+  console.log('Checking types.');
+  const closureCompilerTypeCheck = new ClosureCompiler(
+    Object.assign(commonClosureCompilerSettings, {
+      checks_only: null,
+    }),
+    closureJavaOptions
+  );
+
+  closureCompilerTypeCheck.run(function(exitCode, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+  });
+};
+
+target.buildSimple = function() {
+  console.log('Building the googlejs plugin library with SIMPLE ' +
+              'optimizations.');
+  const closureCompilerBuild = new ClosureCompiler(
+    Object.assign(commonClosureCompilerSettings, {
+      js_output_file: './dist/googlejs-eslint-plugin.js',
+      compilation_level: 'SIMPLE',
+      assume_function_wrapper: null,
+      formatting: 'PRETTY_PRINT',
+      rewrite_polyfills: false,
+    }),
+    closureJavaOptions
+  );
+
+  closureCompilerBuild.run(function(exitCode, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
   });
 };
 
 
+target.buildAdvanced = function() {
+  console.log('Building the googlejs plugin library with ADVANCED ' +
+              'optimizations.');
+  const closureCompilerBuild = new ClosureCompiler(
+    Object.assign(commonClosureCompilerSettings, {
+      js_output_file: './dist/googlejs-eslint-plugin.min.js',
+      compilation_level: 'ADVANCED',
+      use_types_for_optimization: null,
+      rewrite_polyfills: false,
+    }),
+    closureJavaOptions
+  );
+
+  closureCompilerBuild.run(function(exitCode, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+  });
+};
+
 target.lint = function() {
-    let errors = 0,
-        makeFileCache = " ",
-        jsCache = " ",
-        testCache = " ",
-        lastReturn;
+  let errors = 0,
+    makeFileCache = ' ',
+    jsCache = ' ',
+    testCache = ' ',
+    lastReturn;
 
     // using the cache locally to speed up linting process
-    if (!process.env.TRAVIS) {
-        makeFileCache = " --cache --cache-file .cache/makefile_cache ";
-        jsCache = " --cache --cache-file .cache/js_cache ";
-        testCache = " --cache --cache-file .cache/test_cache ";
-    }
+  if (!process.env.TRAVIS) {
+    makeFileCache = ' --cache --cache-file .cache/makefile_cache ';
+    jsCache = ' --cache --cache-file .cache/js_cache ';
+    testCache = ' --cache --cache-file .cache/test_cache ';
+  }
 
-    echo("Linting Makefile.js");
-    lastReturn = exec(ESLINT + makeFileCache + MAKEFILE);
-    if (lastReturn.code !== 0) {
-        errors++;
-    }
+  echo('Linting Makefile.js');
+  lastReturn = exec(ESLINT + makeFileCache + MAKEFILE);
+  if (lastReturn.code !== 0) {
+    errors++;
+  }
 
-    echo("Linting JavaScript files");
-    lastReturn = exec(ESLINT + jsCache + JS_FILES);
-    if (lastReturn.code !== 0) {
-        errors++;
-    }
+  echo('Linting JavaScript files');
+  lastReturn = exec(ESLINT + jsCache + JS_FILES);
+  if (lastReturn.code !== 0) {
+    errors++;
+  }
 
-    echo("Linting JavaScript test files");
-    lastReturn = exec(ESLINT + testCache + TEST_FILES);
-    if (lastReturn.code !== 0) {
-        errors++;
-    }
+  echo('Linting JavaScript test files');
+  lastReturn = exec(ESLINT + testCache + TEST_FILES);
+  if (lastReturn.code !== 0) {
+    errors++;
+  }
 
-    if (errors) {
-        exit(1);
-    }
+  if (errors) {
+    exit(1);
+  }
 };
 
 
 target.test = function() {
-  target.lint();
+  // target.lint();
   // target.checkRuleFiles();
   let errors = 0,
-      lastReturn;
+    lastReturn;
 
   // exec(ISTANBUL + " cover " + MOCHA + "-- -c " + TEST_FILES);
-  lastReturn = nodeCLI.exec("istanbul", "cover", MOCHA, "-- -R progress -t " + MOCHA_TIMEOUT, "-c", TEST_FILES);
+  lastReturn = nodeCLI.exec('istanbul', 'cover', MOCHA, '-- -R progress -t ' + MOCHA_TIMEOUT, '-c', TEST_FILES);
   if (lastReturn.code !== 0) {
     errors++;
   }
 
   // exec(ISTANBUL + "check-coverage --statement 99 --branch 98 --function 99 --lines 99");
-  lastReturn = nodeCLI.exec("istanbul", "check-coverage", "--statement 99 --branch 98 --function 99 --lines 99");
+  lastReturn = nodeCLI.exec('istanbul', 'check-coverage', '--statement 99 --branch 98 --function 99 --lines 99');
   if (lastReturn.code !== 0) {
     errors++;
   }
--- packages/eslint-plugin-googlejs/index.js
+++ packages/eslint-plugin-googlejs/index.js
@@ -3,7 +3,9 @@ goog.module('googlejs.config');
 const inlineCommentSpacing = goog.require('googlejs.rules.inline-comment-spacing');
 const camelcase = goog.require('googlejs.rules.camelcase');
 
-/** @const {!ESLint.Config} */
+/**
+ *  @const {!ESLint.Config}
+ */
 const PLUGIN_ESLINT_CONFIG = {
   rules: {
     'inline-comment-spacing': inlineCommentSpacing,
@@ -12,4 +14,19 @@ const PLUGIN_ESLINT_CONFIG = {
   configs: {},
 };
 
+window['googlejs-eslint-plugin'] = PLUGIN_ESLINT_CONFIG;
+
+// if (typeof define === 'function' && define['amd']) {
+//   // AMD
+//   define(['googlejs-eslint-plugin'], PLUGIN_ESLINT_CONFIG);
+// } else if (typeof require === 'function' && typeof module === 'object' &&
+//            module && module['exports']) {
+//   // CommonJS
+//   module['exports'] = require('googlejs-eslint-plugin');
+// } else {
+//   // Global
+  
+//   goog.global['googlejs-eslint-plugin'] = goog.global['googlejs-eslint-plugin'] || {}
+//   ['ProtoBuf'] = factory(global['googlejs-eslint-plugin']);
+// }
 exports = PLUGIN_ESLINT_CONFIG;
