8a0152c8d264614fa52cddb92679debd8fecfe0c
Joe Schafer <jschaf@google.com>
2016-09-22 00:53:30 -0700

    fix(plugin): appease closure suspicious code checker

--- packages/eslint-plugin-googlejs/tests/utils.js
+++ packages/eslint-plugin-googlejs/tests/utils.js
@@ -13,18 +13,18 @@ const expect = require('chai').expect;
 
 describe('isUnderscored', () => {
   it('should correctly determine underscored strings', () => {
-    expect(utils.isUnderscored('')).to.be.false;
-    expect(utils.isUnderscored('a')).to.be.false;
-    expect(utils.isUnderscored('ab')).to.be.false;
-    expect(utils.isUnderscored('ab c')).to.be.false;
+    expect(utils.isUnderscored('')).to.equal(false);
+    expect(utils.isUnderscored('a')).to.equal(false);
+    expect(utils.isUnderscored('ab')).to.equal(false);
+    expect(utils.isUnderscored('ab c')).to.equal(false);
 
-    expect(utils.isUnderscored('_')).to.be.true;
-    expect(utils.isUnderscored('a_')).to.be.true;
-    expect(utils.isUnderscored('__')).to.be.true;
-    expect(utils.isUnderscored('_a')).to.be.true;
-    expect(utils.isUnderscored(' _')).to.be.true;
-    expect(utils.isUnderscored('abc _')).to.be.true;
-    expect(utils.isUnderscored('_abc')).to.be.true;
+    expect(utils.isUnderscored('_')).to.equal(true);
+    expect(utils.isUnderscored('a_')).to.equal(true);
+    expect(utils.isUnderscored('__')).to.equal(true);
+    expect(utils.isUnderscored('_a')).to.equal(true);
+    expect(utils.isUnderscored(' _')).to.equal(true);
+    expect(utils.isUnderscored('abc _')).to.equal(true);
+    expect(utils.isUnderscored('_abc')).to.equal(true);
   });
 });
 
