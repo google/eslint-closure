352b5ea41712d21de6bca4d8e4d3c6aba9d57422
Joe Schafer <jschaf@google.com>
2017-03-12 15:31:02 -0700

    es6: make config testing work

--- packages/eslint-config-googlejs-es6/test-config.js
+++ packages/eslint-config-googlejs-es6/test-config.js
@@ -3,5 +3,6 @@ const configTester = require('eslint-config-tester');
 configTester.testConfig('./tests/**/*.js', {
   eslintOptions: {
     configFile: './index.js',
+    useEslintrc: false,
   },
 });
