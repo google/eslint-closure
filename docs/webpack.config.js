77281eca68455f00ccb482d1a990f44ee516126c
Joe Schafer <jschaf@google.com>
2017-03-19 15:34:06 -0700

    docs: add webpack dev server

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -1,34 +1,18 @@
 var fs = require('fs');
 var path = require('path');
-var StringReplacePlugin = require('string-replace-webpack-plugin');
 var webpack = require('webpack');
 
 module.exports = {
   entry: './js/app.js',
-
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'app.bundle.js',
+    filename: 'app.min.js',
   },
-
-  plugins: [
-  ],
-
+  plugins: [],
   devtool: 'source-map',
-  resolve: {
-    // you can now require('file') instead of require('file.js')
-    extensions: ['', '.js', '.json']
-  },
   module: {
-    loaders: [
-      {test: /\.json$/, loader: "json"},
+    rules: [
+      {test: /\.json$/, use: [{loader: "json-loader"}]},
     ]
   },
-  // Loaders are resolved relative to the docs directory.  Since the docs/
-  // directory isn't a parent of the plugin directory, we can't use loaders by
-  // default on any requires that eslint-plugin-closure uses.  Setting
-  // resolveLoader avoids this problem.
-  resolveLoader: {
-    root: path.join(__dirname, "node_modules"),
-  }
 };
